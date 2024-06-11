2b5e6141810404137b9fd279f994d968add58c81
Your Name <CHANNY.CHHUN@pnc.lan>
2024-06-11 14:38:00 +0700

    update code register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-
 
 class AuthController extends Controller
 {
@@ -17,7 +17,7 @@ class AuthController extends Controller
             'email' => 'required|email',
             'password' => 'required|string|min:8'
         ]);
-       
+
         $user = User::create($validatedData);
         $token = $user->createToken('mytoken')->plainTextToken;
 
@@ -27,11 +27,23 @@ class AuthController extends Controller
             'token_type' => 'Bearer'
         ]);
     }
+
     public function store(Request $request)
     {
+        // Initialize the $user variable as a new User instance
+        $user = new User();
+        
+        // Assign values to the user's properties
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
+        
+        // Save the user to the database
         $user->save();
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ]);
     }
 }
