761a9e63256ad652177efa9e111d8a4c0835fe6c
maisamorn <samorn.mai@student.passerellesnumeriques.org>
2024-06-11 18:56:33 +0700

    #5 list of posts

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::resource('/post',PostController::class);
