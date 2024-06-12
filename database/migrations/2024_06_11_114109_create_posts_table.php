a5945b2ac11e12236f66955453d6f4e791e6b84d
maisamorn <samorn.mai@student.passerellesnumeriques.org>
2024-06-12 08:43:58 +0700

    #6 update show post

--- database/migrations/2024_06_11_114109_create_posts_table.php
+++ database/migrations/2024_06_11_114109_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
+            $table->text('image');
             $table->foreignId("user_id")
             ->references("id")
             ->on("users")
